e0434e4e2632f6286ec6061cb74f82a5c5fc21ae
anitnyd <122319157+anitnyd@users.noreply.github.com>
2023-02-15 23:37:11 +0900

    Add files via upload

---
+++ Level4/Lv4-3.c
@@ -0,0 +1,29 @@
+/*문제
+N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+출력
+첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n = '\0', i;
+	int ary[1000001];
+	int max=-1000000, min=1000000;		//추후 무조건 바뀔 수 밖에 없는 설정.
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", &ary[i]);
+	for (i = 0; i < n; i++)
+	{
+		if (ary[i] > max) max = ary[i];
+		if (ary[i] < min) min = ary[i];
+	}
+
+	printf("%d %d", min, max);
+	return 0;
+}
