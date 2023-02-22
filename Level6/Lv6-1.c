436e88ed96cb95986af82f7dc20422971e1e1275
anitnyd <122319157+anitnyd@users.noreply.github.com>
2023-02-22 21:40:29 +0900

    Add files via upload

---
+++ Level6/Lv6-1.c
@@ -0,0 +1,39 @@
+/*문제
+아래 예제와 같이 새싹을 출력하시오.
+
+입력
+입력은 없다.
+
+출력
+새싹을 출력한다.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+int main(void)
+{
+	puts("         ,r'\"7");
+	puts("r`-_   ,'  ,/");
+	puts(" \\. \".L_r'");
+	puts("   `~\\/");
+	puts("      |");
+	printf("      |");
+
+	return 0;
+}
+*/
+//다른 방법
+
+int main(void)
+{
+	printf("         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf(" \\. \".L_r'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |");
+
+	return 0;
+}
+
